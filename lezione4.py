0d1e2980bc87b886a4c96f46ef5c40d4bec90f55
santofrancesco <66701394+Francesco-Santoianni@users.noreply.github.com>
2021-12-10 12:40:09 +0000

    -Aggiunti commenti per lezione4.py

--- lezione4.py
+++ lezione4.py
@@ -1,8 +1,11 @@
+#definisco una classe
 class CSVFILE:
     
+    #inizializzo sul nome stesso del file e definisco gli oggetti
     def __init__(shampoo_sales, name):
         shampoo_sales.name = name
 
+    #creo una funzione get_data() che mi torna i valori di shampoo_sales.txt come lista di liste
     def get_data(shampoo_sales):
         datalist1 = []
         with open('shampoo_sales.txt') as x:
